6a251ebca96f52bba62cc7a224602183d34eae69
SasiKumar <sasimathiru97@gmail.com>
2024-12-22 17:29:13 +0530

    Sasikumar commit Java Variables

---
+++ JavaVariables/src/org/var/Variable.java
@@ -0,0 +1,29 @@
+package org.var;
+//Java Variables
+public class Variable {
+	public static void main(String[] args) {
+		byte empAge=25;
+		System.out.println("Employee Age is : " + empAge);
+		short id=123;
+		System.out.println("Employee Id is : " + id);
+		int empPincode=6000987;
+		System.out.println("Employee pincode is : " + empPincode);
+		long empPhone=90873738839l; // Type Cast
+		System.out.println("Employee Phone is : " + empPhone);
+		float empSalary=987.988f;
+		System.out.println("Employee Salary is : " + empSalary);
+		float x=20/3f;
+		System.out.println(x);
+		double empAnswer=899.33223d;
+		System.out.println("Employee Answer is : " + empAnswer);
+		double y=20/3d;
+		System.out.println(y);
+		char c='M';
+		System.out.println("Employee gender is : " + c);
+		String empMail="Greens@123.com";
+		System.out.println("Employee Mail is : " + empMail);
+		boolean status=true;
+		System.out.println("Emplpyee Marital Status is " + status);
+	} 
+
+}
